af3c97a8d1ce0aa6754cc1df9afc8a8a481535e3
Munawar Ali <munawar.ali2012@gmail.com>
2018-07-25 19:45:59 +0500

    First Spring MVC commit. Setuping Project. Till video 6

---
+++ springmvc/src/main/java/org/amunawar/controller/HelloWorldController.java
@@ -0,0 +1,24 @@
+package main.java.org.amunawar.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by sheik on 7/25/2018.
+ */
+public class HelloWorldController  extends AbstractController{
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+        System.out.println("------------------------------Hello");
+        ModelAndView modelAndView = new ModelAndView("HelloPage");
+        modelAndView.addObject("welcomeMessage","Hi user, welcome to the project Spring MVC");
+        return modelAndView;
+    }
+
+
+}
