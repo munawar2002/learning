44824d06acfb020be4ae0c0c8fb0c7b7268bdcd8
Munawar Ali <munawar.ali2012@gmail.com>
2018-07-26 02:25:05 +0500

    Done RequestMapping and PathVariable. Start with 10 video.

--- springmvc/src/main/java/org/amunawar/controller/HelloWorldController.java
+++ springmvc/src/main/java/org/amunawar/controller/HelloWorldController.java
@@ -1,23 +1,29 @@
 package main.java.org.amunawar.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * Created by sheik on 7/25/2018.
  */
-public class HelloWorldController  extends AbstractController{
+@Controller
+@RequestMapping("/springmvc")
+public class HelloWorldController{
 
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request,
-                                                 HttpServletResponse response) throws Exception {
+    @RequestMapping("/welcome/{countryName}/{userName}")
+    protected ModelAndView helloWorld(@PathVariable Map<String,String> pathVars) throws Exception {
+        String name = pathVars.get("userName");
+        String country = pathVars.get("countryName");
+
         ModelAndView modelAndView = new ModelAndView("HelloPage");
-        modelAndView.addObject("welcomeMessage","Hi user, welcome to the project Spring MVC");
+        modelAndView.addObject("welcomeMessage","Hi "+name+", welcome to "+country);
         return modelAndView;
     }
-
-
 }
